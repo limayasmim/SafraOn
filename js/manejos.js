b03db5fa4f518fa14d604df5ceb6689217b2ee89
Yasmim Lima <limaceremyasmim@gmail.com>
2025-11-23 23:23:02 -0300

    resposividade login

--- js/manejos.js
+++ js/manejos.js
@@ -164,7 +164,7 @@ class GerenciadorManejos {
             const novoItem = document.createElement('article');
             novoItem.className = 'item-dado';
             novoItem.dataset.index = index;
-            novoItem.style.gridTemplateColumns = '1fr 1fr 1fr 1fr';
+            // novoItem.style.gridTemplateColumns = '1fr 1fr 1fr 1fr';
 
             novoItem.innerHTML = `
             <span>${item.data || ''}</span>
